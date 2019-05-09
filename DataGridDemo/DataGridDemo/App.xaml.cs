b04afa82a7c220746536de22b76119ca89526930
pradeepkumar417 <princepradeep417@gmail.com>
2019-05-09 17:33:23 +0530

    Sample is committed

---
+++ DataGridDemo/DataGridDemo/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace DataGridDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+
+            //Register Syncfusion license
+            //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTI0MzlAMzEzNjJlMzQyZTMwTU5PRWlmc0hTRzlzMnpibFFLakpnWUVxd1JsVUw2WW9tdnFmT0dycHEwVT0=");
+
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ DataGridDemo/DataGridDemo/Model/OrderInfo.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridDemo
+{
+    public class OrderInfo : INotifyPropertyChanged
+    {
+        public OrderInfo()
+        {
+
+        }
+
+        #region private variables
+
+
+        private int _employeeID;
+        private string _customerID;
+        private string _firstname;
+        private string _lastname;
+        private string _gender;
+        private string _shipCity;
+        private string _shipCountry;
+        private double _freight;
+        private DateTime? _shippingDate;
+        private bool _isClosed;
+
+        #endregion
+
+        #region Public Properties
+
+        private int _orderID;
+        public int OrderID
+        {
+            get
+            {
+                return _orderID;
+            }
+            set
+            {
+                this._orderID = value;
+                RaisePropertyChanged("OrderID");
+            }
+        }
+
+
+        public int EmployeeID
+        {
+            get
+            {
+                return _employeeID;
+            }
+            set
+            {
+                this._employeeID = value;
+                RaisePropertyChanged("EmployeeID");
+            }
+        }
+
+        public string CustomerID
+        {
+            get
+            {
+                return _customerID;
+            }
+            set
+            {
+                this._customerID = value;
+                RaisePropertyChanged("CustomerID");
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstname;
+            }
+            set
+            {
+                this._firstname = value;
+                RaisePropertyChanged("FirstName");
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastname;
+            }
+            set
+            {
+                this._lastname = value;
+                RaisePropertyChanged("LastName");
+            }
+        }
+
+        public string Gender
+        {
+            get
+            {
+                return _gender;
+            }
+            set
+            {
+                this._gender = value;
+                RaisePropertyChanged("Gender");
+            }
+        }
+
+        public string ShipCity
+        {
+            get
+            {
+                return _shipCity;
+            }
+            set
+            {
+                this._shipCity = value;
+                RaisePropertyChanged("ShipCity");
+            }
+        }
+
+        public string ShipCountry
+        {
+            get
+            {
+                return _shipCountry;
+            }
+            set
+            {
+                this._shipCountry = value;
+                RaisePropertyChanged("ShipCountry");
+            }
+        }
+
+        public double Freight
+        {
+            get
+            {
+                return _freight;
+            }
+            set
+            {
+                this._freight = value;
+                RaisePropertyChanged("Freight");
+            }
+        }
+
+
+        private bool enabled;
+
+        public bool Enabled
+        {
+            get
+            {
+                return enabled;
+            }
+            set
+            {
+                enabled = value;
+                RaisePropertyChanged("Enabled");
+            }
+        }
+
+
+        public bool IsClosed
+        {
+            get
+            {
+                return _isClosed;
+            }
+            set
+            {
+                this._isClosed = value;
+                RaisePropertyChanged("IsClosed");
+            }
+        }
+
+        public DateTime? ShippingDate
+        {
+            get
+            {
+                return _shippingDate;
+            }
+            set
+            {
+                this._shippingDate = value;
+                RaisePropertyChanged("ShippingDate");
+            }
+        }
+        //public object this[string _propertyName]
+        //{
+        //    get
+        //    {
+        //        return "Test";
+        //    }
+
+        //    set
+        //    {
+        //    }
+        //}
+
+        #endregion
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(String Name)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(Name));
+        }
+
+        #endregion
+    }
+}
